e750d984c2caddb52a91fa94c50f5d5cabe09e40
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2021-09-27 16:54:51 +0100

    almost done

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 'use strict';
+const rollBtn = document.querySelector('.btnClick');
+
+rollBtn.addEventListener('click', DiceRoll);
+
+function DiceRoll() {
+    
+        
+        var roll1 = Math.trunc(Math.random() * 6) + 1
+        var roll2 = Math.trunc(Math.random() * 6) + 1
+
+
+        document.querySelector(".dice_img_1").setAttribute("src",
+            "dice" + roll1 + ".png");
+
+        document.querySelector(".dice_img_2").setAttribute("src",
+            "dice" + roll2 + ".png");
+
+        if (roll1 === roll2) {
+            document.querySelector("h3").innerHTML = "Draw!";
+            
+        } else if (roll1 < roll2) {
+            document.querySelector("h3").innerHTML = (" Morgan wins!");
+            
+        } else {
+            document.querySelector("h3").innerHTML = (" you win!");
+            
+            document.querySelector("h4").innerHTML = score;
+        }
+    /*
+    function scoreAdd() {
+        var score = 0;
+        if (roll1 > roll2) {
+            score++;
+            document.querySelector("h4").innerHTML = score;
+        } else if (roll1 < roll2) {
+            score--;
+            document.querySelector("h4").innerHTML = score;
+        } else {
+            score = score;
+            document.querySelector("h4").innerHTML = score;
+        }
+        
+    }
+    scoreAdd();
+    */
+        
+    
+}
