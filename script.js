bca90852bea38fe9a29d80b8b472c1449a0e1951
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2021-09-27 17:23:03 +0100

    finished...kinda

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 'use strict';
 const rollBtn = document.querySelector('.btnClick');
-
 rollBtn.addEventListener('click', DiceRoll);
 
-function DiceRoll() {
-    
-        
-        var roll1 = Math.trunc(Math.random() * 6) + 1
-        var roll2 = Math.trunc(Math.random() * 6) + 1
 
+function DiceRoll(roll1, roll2) {
 
-        document.querySelector(".dice_img_1").setAttribute("src",
-            "dice" + roll1 + ".png");
+    var roll1 = Math.trunc(Math.random() * 6) + 1
+    var roll2 = Math.trunc(Math.random() * 6) + 1
 
-        document.querySelector(".dice_img_2").setAttribute("src",
-            "dice" + roll2 + ".png");
+    document.querySelector(".dice_img_1").setAttribute("src",
+        "dice" + roll1 + ".png");
 
-        if (roll1 === roll2) {
-            document.querySelector("h3").innerHTML = "Draw!";
-            
-        } else if (roll1 < roll2) {
-            document.querySelector("h3").innerHTML = (" Morgan wins!");
-            
-        } else {
-            document.querySelector("h3").innerHTML = (" you win!");
-            
-            document.querySelector("h4").innerHTML = score;
-        }
-    /*
-    function scoreAdd() {
-        var score = 0;
+    document.querySelector(".dice_img_2").setAttribute("src",
+        "dice" + roll2 + ".png");
+
+    if (roll1 === roll2) {
+        document.querySelector("h3").innerHTML = "Draw!";
+
+    } else if (roll1 < roll2) {
+        document.querySelector("h3").innerHTML = (" Morgan wins!");
+
+    } else {
+        document.querySelector("h3").innerHTML = (" You win!");
+
+        document.querySelector("h4").innerHTML = score;
+    }
+
+    function scoreAdd(roll1, roll2) {
+        let score = 0;
         if (roll1 > roll2) {
             score++;
             document.querySelector("h4").innerHTML = score;
@@ -40,10 +38,13 @@ function DiceRoll() {
             score = score;
             document.querySelector("h4").innerHTML = score;
         }
-        
+
+
+
     }
-    scoreAdd();
-    */
-        
+
+
     
+
+
 }
